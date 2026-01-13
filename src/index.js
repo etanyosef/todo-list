968cdb05d9a11d9fac3806311f47ac75b69f0cfc
Ethan <ethancredo.ascc@outlook.com>
2026-01-13 11:23:21 +0800

    add event listener to task buttons and render pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import './css/style.css';
 import './css/reset.css';
 
-import renderTasks from './app/tasks.js';
+import renderTasksToday from './app/tasks/today.js';
+import renderTasksWeek from './app/tasks/week.js';
 
-renderTasks();
+renderTasksToday();
+
+const btnToday = document.getElementById('btn-today');
+const btnWeek = document.getElementById('btn-week');
+const btnMonth = document.getElementById('btn-month');
+
+btnToday.addEventListener('click', () => {
+    renderTasksToday();
+});
+
+btnWeek.addEventListener('click', () => {
+    renderTasksWeek();
+});
+
