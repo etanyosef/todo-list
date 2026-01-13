4923eb3530c1bd8894081a84c59f4fe48d40546d
Ethan <ethancredo.ascc@outlook.com>
2026-01-13 16:52:27 +0800

    create task dialog form

--- src/app/tasks/today.js
+++ src/app/tasks/today.js
@@ -1,17 +1,21 @@
 import { myTasks } from './myTasks.js';
 
+const dialog = document.createElement('dialog');
+
 export default function renderTasksToday() {
-    // set page title
+    const mainContent = document.querySelector('.content');
+    // clear and set page title
     const pageTitleDiv = document.querySelector('.page-title');
+
     const pageTitleH2 = document.createElement('h2');
 
     pageTitleDiv.textContent = '';  
     pageTitleH2.textContent = 'Today';
+    mainContent.textContent = '';
 
     pageTitleDiv.prepend(pageTitleH2);
 
     // display tasks to DOM
-    const mainContent = document.querySelector('.content');
     const taskContainerDiv = document.createElement('div');
     
     taskContainerDiv.classList.add('task-container');    
@@ -24,6 +28,10 @@ export default function renderTasksToday() {
         const taskPriority = document.createElement('span');    
         
         taskDiv.classList.add('task');
+        taskTitle.classList.add('title');
+        taskDescription.classList.add('description');
+        taskDueDate.classList.add('due-date');
+        taskPriority.classList.add('priority');
         
         taskTitle.textContent = task.title;
         taskDescription.textContent = task.description;
@@ -38,7 +46,157 @@ export default function renderTasksToday() {
         taskContainerDiv.append(taskDiv);
     });
 
+    // add new task button
+    const addTaskBtn = document.createElement('button');
+    addTaskBtn.classList.add('btn-new-task');
+    addTaskBtn.textContent = 'New task';
+    addTaskBtn.addEventListener('click', () => showAddTaskDialog());
+    taskContainerDiv.prepend(addTaskBtn);
+
     mainContent.append(taskContainerDiv);
 
-    console.log(myTasks);
+    createAddTaskDialog();
+    mainContent.append(dialog);
+}
+
+const createAddTaskDialog = () => {
+    // dialog header
+    const dialogHead = document.createElement('div');
+    const dialogTitle = document.createElement('h2');
+    const dialogCloseBtn = document.createElement('button');
+
+    dialogTitle.textContent = 'New Task';
+    dialogCloseBtn.textContent = 'Close';
+    dialogCloseBtn.addEventListener('click', () => {
+        document.querySelector('dialog form').reset();
+        dialog.close();
+    });
+
+    dialogHead.append(dialogTitle);
+    dialogHead.append(dialogCloseBtn);
+
+    // create form
+    const form = document.createElement('form');
+
+    // task title label and input
+    const titleDiv = document.createElement('div');
+    const titleLabel = document.createElement('label');
+    const titleInput = document.createElement('input');
+
+    titleDiv.classList.add('form-group');
+    titleLabel.textContent = 'Title';
+    titleLabel.htmlFor = 'task-title';
+    titleInput.type = 'text';
+    titleInput.id = 'task-title';
+    titleInput.required = true;
+
+    titleDiv.append(titleLabel);
+    titleDiv.append(titleInput);
+    form.append(titleDiv);
+    
+    // task description label and input
+    const descriptionDiv = document.createElement('div');
+    const descriptionLabel = document.createElement('label');
+    const descriptionInput = document.createElement('input');
+
+    descriptionDiv.classList.add('form-group');
+    descriptionLabel.textContent = 'Description';
+    descriptionLabel.htmlFor = 'task-description';
+    descriptionInput.type = 'text';
+    descriptionInput.id = 'task-description';
+    descriptionInput.required = true;
+
+    descriptionDiv.append(descriptionLabel);
+    descriptionDiv.append(descriptionInput);
+    form.append(descriptionDiv);
+
+    // task due date label and input
+    const dueDateDiv = document.createElement('div');
+    const dueDateLabel = document.createElement('label');
+    const dueDateInput  = document.createElement('input');
+
+    dueDateDiv.classList.add('form-group');
+    dueDateLabel.textContent = 'Due Date';
+    dueDateLabel.htmlFor = 'task-due-date';
+    dueDateInput.type = 'date';
+    dueDateInput.id = 'task-due-date';
+    dueDateInput.required = true;
+
+    dueDateDiv.append(dueDateLabel);
+    dueDateDiv.append(dueDateInput);
+    form.append(dueDateDiv);
+
+    // task priority fieldset and radios
+    const priorityFieldset = document.createElement('fieldset');
+    const priorityLegend = document.createElement('legend');
+    priorityLegend.textContent = 'Priority';
+    priorityFieldset.classList.add('fieldset');
+    priorityFieldset.required = true;
+    priorityFieldset.append(priorityLegend);
+
+    // priority low radio
+    const priorityLowLabel = document.createElement('label');
+    const priorityLowRadio = document.createElement('input');
+
+    priorityLowLabel.textContent = 'Low';
+    priorityLowLabel.htmlFor = 'priority-low';
+
+    priorityLowRadio.type = 'radio';
+    priorityLowRadio.name = 'priority';
+    priorityLowRadio.id = 'priority-low';
+    priorityLowRadio.value = 'Low';
+    priorityLowRadio.required = true;
+    
+    priorityLowLabel.append(priorityLowRadio);
+    priorityFieldset.append(priorityLowLabel);    
+
+    // priority medium radio
+    const priorityMediumLabel = document.createElement('label');
+    const priorityMediumRadio = document.createElement('input');
+
+    priorityMediumLabel.textContent = 'Medium';
+    priorityMediumLabel.htmlFor = 'priority-medium';
+
+    priorityMediumRadio.type = 'radio';
+    priorityMediumRadio.name = 'priority';
+    priorityMediumRadio.id = 'priority-medium';
+    priorityMediumRadio.value = 'Medium';
+
+    priorityMediumLabel.append(priorityMediumRadio);
+    priorityFieldset.append(priorityMediumLabel);
+
+    // priority high radio
+    const priorityHighLabel = document.createElement('label');
+    const priorityHighRadio = document.createElement('input');
+
+    priorityHighLabel.textContent = 'High';
+    priorityHighLabel.htmlFor = 'priority-high';
+
+    priorityHighRadio.type = 'radio';
+    priorityHighRadio.name = 'priority';
+    priorityHighRadio.id = 'priority-high';
+    priorityHighRadio.value = 'High';
+
+    priorityHighLabel.append(priorityHighRadio);
+    priorityFieldset.append(priorityHighLabel);
+    form.append(priorityFieldset);
+
+    // form buttons
+    const buttonDiv = document.createElement('div');
+    const buttonAddTask = document.createElement('button');
+
+    buttonDiv.classList.add('form-group');
+    buttonAddTask.textContent = 'Add Task';
+    buttonAddTask.type = 'submit';
+
+    buttonDiv.append(buttonAddTask);
+    form.append(buttonDiv);
+
+    // append dialog header and form to dialog
+    dialog.append(dialogHead);
+    dialog.append(form);
+}
+
+const showAddTaskDialog = () => {
+    dialog.showModal();
 }
