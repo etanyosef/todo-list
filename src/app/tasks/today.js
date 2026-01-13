ad8c5c4014c055a089ddf33bfa7a8e1816da642f
Ethan <ethancredo.ascc@outlook.com>
2026-01-13 11:56:22 +0800

    display tasks in DOM

--- src/app/tasks/today.js
+++ src/app/tasks/today.js
@@ -10,7 +10,35 @@ export default function renderTasksToday() {
 
     pageTitleDiv.prepend(pageTitleH2);
 
-    // 
+    // display tasks to DOM
+    const mainContent = document.querySelector('.content');
+    const taskContainerDiv = document.createElement('div');
+    
+    taskContainerDiv.classList.add('task-container');    
+
+    myTasks.tasks.forEach(task => {
+        const taskDiv = document.createElement('div');
+        const taskTitle = document.createElement('span');
+        const taskDescription = document.createElement('span');
+        const taskDueDate = document.createElement('span');
+        const taskPriority = document.createElement('span');    
+        
+        taskDiv.classList.add('task');
+        
+        taskTitle.textContent = task.title;
+        taskDescription.textContent = task.description;
+        taskDueDate.textContent = task.dueDate;
+        taskPriority.textContent = task.priority;
+
+        taskDiv.append(taskTitle);
+        taskDiv.append(taskDescription);
+        taskDiv.append(taskDueDate);
+        taskDiv.append(taskPriority);
+
+        taskContainerDiv.append(taskDiv);
+    });
+
+    mainContent.append(taskContainerDiv);
 
     console.log(myTasks);
 }
