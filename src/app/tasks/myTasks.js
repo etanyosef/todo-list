ed8966e12fd3a3644d8742dba4da772236d986d9
Ethan <ethancredo.ascc@outlook.com>
2026-01-13 11:22:27 +0800

    create task and tasks class

---
+++ src/app/tasks/myTasks.js
@@ -0,0 +1,23 @@
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+class Tasks {
+    constructor() {
+        this.tasks = [];
+    }
+
+    newTask(title, description, dueDate, priority) {
+        const task = new Task(title, description, dueDate, priority);
+        this.tasks.push(task);
+    }
+}
+
+export const myTasks = new Tasks();
+myTasks.newTask('awaw', 'wawaawewe', 'karun', 'ambot');
+
