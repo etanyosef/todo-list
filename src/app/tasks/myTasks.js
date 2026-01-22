5a8a53f80b2e034ca996f7c52e8fa20dcd25631a
Ethan <ethancredo.ascc@outlook.com>
2026-01-22 11:43:27 +0800

    export Project class

--- src/app/tasks/myTasks.js
+++ src/app/tasks/myTasks.js
@@ -45,7 +45,7 @@ myTasks.newTask('awaw2', 'wawaawewe2', 'karun', 'high');
 myTasks.newTask('awaw2', 'wawaawewe2', 'karun', 'medium');
 
 export const Projects = [];
-class Project {
+export class Project {
     constructor(name) {
         this.id = crypto.randomUUID;
         this.name = name;
