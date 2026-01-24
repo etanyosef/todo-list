3d53a9383498f17ad509b3dbb21fa4a200fa23ab
Ethan <ethancredo.ascc@outlook.com>
2026-01-24 09:23:09 +0800

    change close button to svg

--- src/app/tasks/renderProject.js
+++ src/app/tasks/renderProject.js
@@ -19,10 +19,12 @@ export const renderProjectMenu = () => {
         const projectLi = document.createElement('li');
         const projectBtn = document.createElement('button');
         const deleteProjectBtn = document.createElement('button');
+        const svgDeleteCode = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete-forever</title><path d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8.46,11.88L9.87,10.47L12,12.59L14.12,10.47L15.53,11.88L13.41,14L15.53,16.12L14.12,17.53L12,15.41L9.88,17.53L8.47,16.12L10.59,14L8.46,11.88M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" /></svg>';
+        
 
         projectBtn.textContent = project.name;
         projectBtn.classList.add('add-btn');
-        deleteProjectBtn.textContent = 'x';
+        deleteProjectBtn.innerHTML = svgDeleteCode;
         deleteProjectBtn.classList.add('delete-btn');
 
         projectLi.prepend(projectBtn);
