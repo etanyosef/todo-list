6a4d90f0fa4922667bf7067a530d948f9f1750d8
Ethan <ethancredo.ascc@outlook.com>
2026-01-26 15:16:32 +0800

    add icon to new project button

--- src/app/tasks/renderProject.js
+++ src/app/tasks/renderProject.js
@@ -10,8 +10,11 @@ const pageTitleH2 = document.createElement('h2');
 export const renderProjectMenu = () => {
     const projectsMenuList = document.querySelector('.projects .sidemenu');
     projectsMenuList.textContent = '';
+
     const projectLi = document.createElement('li');
     const projectBtn = document.createElement('button');
+    const newProjectSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>folder-plus</title><path d="M13 19C13 19.34 13.04 19.67 13.09 20H4C2.9 20 2 19.11 2 18V6C2 4.89 2.89 4 4 4H10L12 6H20C21.1 6 22 6.89 22 8V13.81C21.12 13.3 20.1 13 19 13C15.69 13 13 15.69 13 19M20 18V15H18V18H15V20H18V23H20V20H23V18H20Z" /></svg>';
+    const newProjectIcon = document.createElement('span');
 
     projectBtn.classList.add('btn-new-project');
 
@@ -59,11 +62,13 @@ export const renderProjectMenu = () => {
             renderTasksToday();
         });
     });
-
+    
+    newProjectIcon.innerHTML = newProjectSvg;
     projectLi.append(projectBtn);
     projectsMenuList.prepend(projectLi);
 
-    projectBtn.textContent = '+ New Project';
+    projectBtn.textContent = 'New Project';
+    projectBtn.prepend(newProjectIcon);
     projectBtn.addEventListener('click', () => {
         renderAddProjectDialog();
     });
