c1afdbef26e2e52a901363f7ef4d41e4959b89e2
Ethan <ethancredo.ascc@outlook.com>
2026-01-29 14:14:27 +0800

    refactor local storage and cleanup code

--- src/app/tasks/localStorage.js
+++ src/app/tasks/localStorage.js
@@ -1,6 +1,3 @@
-import { myTasks, Projects } from './myTasks.js';
-
-
 export const myLocalStorage = {
     set(data) {
         localStorage.setItem('myTasks', JSON.stringify(data));
@@ -12,18 +9,3 @@ export const myLocalStorage = {
     }
 }
 
-export const setTasksToStorage = (myTasks) => {
-    localStorage.setItem('myTasks', JSON.stringify(myTasks));
-}
-
-export const getTasksFromStorage = () => {
-    const storageMyTasks = JSON.parse(localStorage.getItem('myTasks'));
-    return storageMyTasks;
-}
-
-localStorage.setItem('Projects', JSON.stringify(Projects));
-
-
-
-const storageProjects = JSON.parse(localStorage.getItem('Projects'));
-console.log(storageProjects);
