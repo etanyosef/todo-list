4f76c3049e803a4a286cf764e1f00c158004eaba
Ethan <ethancredo.ascc@outlook.com>
2026-02-09 15:11:06 +0800

    add svg icon for project name edit

--- src/app/tasks/renderProject.js
+++ src/app/tasks/renderProject.js
@@ -42,7 +42,9 @@ export const renderProjectMenu = () => {
 
             // edit project title button
             const editProjectTitleBtn = document.createElement('button');
-            editProjectTitleBtn.textContent = 'Edit Title';            
+            const svgEditCode = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Edit</title><path d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z" /></svg>';
+            
+            editProjectTitleBtn.innerHTML = svgEditCode;
             pageTitleDiv.append(editProjectTitleBtn);
 
             pageTitleDiv.prepend(pageTitleH2);
@@ -153,8 +155,7 @@ const renderProjectDialog = (btn, project) => {
         e.preventDefault();
 
         if (btn == 'Add') {
-            Projects.push(new Project(crypto.randomUUID(), projectNameInput.value, []));
-            
+            Projects.push(new Project(crypto.randomUUID(), projectNameInput.value, []));            
             renderProjectMenu();
         } else if (btn == 'Edit') {
             project.name = projectNameInput.value;            
